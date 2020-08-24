3392639b439c3dc8a73316a6ebb40b3e4a08b908
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-23 20:45:24 -0500

    Import Spotify vuex module and persistedstate plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-// import example from './module-example'
+import createPersistedState from 'vuex-persistedstate'
+import spotify from './spotify'
 
 Vue.use(Vuex)
 
@@ -17,9 +17,11 @@ Vue.use(Vuex)
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      spotify
     },
-
+    plugins: [createPersistedState({
+      storage: window.sessionStorage
+    })],
     // enable strict mode (adds overhead!)
     // for dev mode only
     strict: process.env.DEV
