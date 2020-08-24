497ab03fa398f7687381386e6e77fa9a960cbf92
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-23 20:43:14 -0500

    Set authroization header to token from vuex state

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+export default function ({ store }) {
+  axios.defaults.headers.common.Authorization = `Bearer ${store.state.spotify.token}`
+}
+
 Vue.prototype.$axios = axios
