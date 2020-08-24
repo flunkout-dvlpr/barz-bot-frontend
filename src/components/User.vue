13b0670e1175efde2cf5910b0ddf6374ae4fe911
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-23 20:48:16 -0500

    User component to display information related to spotify user, display name/subscription type/followers etc.

---
+++ src/components/User.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-card-section>
+    <q-item class="bg-dark" style="border-top-left-radius: 10px; border-top-right-radius: 10px;">
+      <q-item-section avatar>
+        <q-btn round type="a" :href="userLink">
+          <q-avatar color="dark" round size="64px">
+            <img v-if="userImage" :src="userImage">
+            <q-icon v-else name="person" color="secondary" />
+          </q-avatar>
+        </q-btn>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label class="text-h6 text-white">
+          <q-badge align="middle" color="secondary" class="text-dark"> {{ userSubcription }}</q-badge>
+          <br>
+          {{ userName }}
+        </q-item-label>
+        <q-item-label caption class="text-white"> {{ userEmail }}</q-item-label>
+      </q-item-section>
+
+      <q-item-section side>
+        <q-item-label caption class="text-white">Followers: {{ userFollowers }}</q-item-label>
+        <q-item-label caption class="text-white">Country: {{ userCountry }}</q-item-label>
+      </q-item-section>
+
+    </q-item>
+  </q-card-section>
+</template>
+<script>
+export default {
+  name: 'User',
+  props: {
+    user: Object
+  },
+  computed: {
+    userName () {
+      if (this.user) {
+        return this.user.display_name
+      } else {
+        return null
+      }
+    },
+    userLink () {
+      if (this.user) {
+        return this.user.external_urls.spotify
+      } else {
+        return null
+      }
+    },
+    userImage () {
+      if (this.user) {
+        return this.user.images[0].url
+      } else {
+        return null
+      }
+    },
+    userSubcription () {
+      if (this.user) {
+        return this.user.product
+      } else {
+        return null
+      }
+    },
+    userEmail () {
+      if (this.user) {
+        return this.user.email
+      } else {
+        return null
+      }
+    },
+    userFollowers () {
+      if (this.user) {
+        return this.user.followers.total
+      } else {
+        return null
+      }
+    },
+    userCountry () {
+      if (this.user) {
+        return this.user.country
+      } else {
+        return null
+      }
+    }
+  }
+}
+</script>
+<style>
+</style>
