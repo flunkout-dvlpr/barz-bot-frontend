681043aab43be812668f6c8620789a14e0d6efa2
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-24 13:32:20 -0500

    Update redirect uri

--- src/store/spotify/state.js
+++ src/store/spotify/state.js
@@ -1,6 +1,6 @@
 export default function () {
   return {
-    redirect_uri: 'https://s3.us-east-2.amazonaws.com/www.barz-bot.com/index.html#/',
+    redirect_uri: 'https://s3.us-east-2.amazonaws.com/www.barz-bot.com/index.html/',
     client_id: 'ae6a056b5a2244b8b30e0a2cc3300c44',
     client_secret: '31ffe8ba96ea4b8aa7f67071c8e92d85',
     token: null,
