0250f93e9eedb6c2f611fd6b5d38f6d7b106960e
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-23 21:30:06 -0500

    Conditionally set redirect url based on env prod or dev

--- src/store/spotify/state.js
+++ src/store/spotify/state.js
@@ -1,6 +1,6 @@
 export default function () {
   return {
-    redirect_uri: 'https://s3.us-east-2.amazonaws.com/www.barz-bot.com/index.html',
+    redirect_uri: process.env.PROD ? 'https://s3.us-east-2.amazonaws.com/www.barz-bot.com/index.html' : 'http://localhost:8082/',
     client_id: 'ae6a056b5a2244b8b30e0a2cc3300c44',
     client_secret: '31ffe8ba96ea4b8aa7f67071c8e92d85',
     token: null,
