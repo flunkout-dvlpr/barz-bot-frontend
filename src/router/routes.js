1070c5cff80e4dc207328f0cf38ab712d059a1b6
flunkout-dvlpr <info@juliogonzalez.space>
2021-03-19 18:32:12 -0500

    Create route for artowrok display page with path parameters

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/artwork/:spotifyUser/:imageId', component: () => import('pages/Artwork.vue') }
     ]
   },
 
