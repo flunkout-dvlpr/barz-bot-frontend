09b16c0b8fb72676e8cbd0c595bce15a0c971291
flunkout-dvlpr <info@juliogonzalez.space>
2021-03-19 18:35:59 -0500

    Remove .jpg from generated url

--- src/pages/Artwork.vue
+++ src/pages/Artwork.vue
@@ -2,7 +2,7 @@
   <q-page class="column flex flex-center bg-primary" style="height: auto;">
     <q-card class="q-my-md bg-accent" style="width: 350px; border-radius: 10px;">
       <q-img
-        :src="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}.jpg`"
+        :src="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}`"
       />
     </q-card>
   </q-page>
