7cfd85e835fe3efdeeeb89fcf6cc77b46c34a546
flunkout-dvlpr <info@juliogonzalez.space>
2021-03-19 18:49:05 -0500

    Add metadata for twitter card

--- src/pages/Artwork.vue
+++ src/pages/Artwork.vue
@@ -4,6 +4,14 @@
       <q-img
         :src="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}`"
       />
+      <meta name="twitter:card" content="summary_large_image">
+      <meta name="twitter:site" content="@site_username">
+      <meta name="twitter:title" content="Top 10 Things Ever">
+      <meta name="twitter:description" content="Up than 200 characters.">
+      <meta name="twitter:creator" content="@creator_username">
+      <!-- <meta name="twitter:image" content="http://placekitten.com/250/250"> -->
+      <meta name="twitter:image" :content="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}`">
+      <meta name="twitter:domain" :content="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}`">
     </q-card>
   </q-page>
 </template>
