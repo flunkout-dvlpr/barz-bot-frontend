e12c60a620d0091dfb84eee5b0ead69e912f2ee2
flunkout-dvlpr <info@juliogonzalez.space>
2021-03-19 18:32:54 -0500

    Create page to display artwork using spotify username and image as in s3 bucket

---
+++ src/pages/Artwork.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="column flex flex-center bg-primary" style="height: auto;">
+    <q-card class="q-my-md bg-accent" style="width: 350px; border-radius: 10px;">
+      <q-img
+        :src="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}.jpg`"
+      />
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Artwork',
+  created () {
+    this.spotifyUser = this.$route.params.spotifyUser
+    this.imageId = this.$route.params.imageId
+  }
+}
+</script>
