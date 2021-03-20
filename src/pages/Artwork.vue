3adbe5837398ff93b458665d4db1a220785c93f6
flunkout-dvlpr <info@juliogonzalez.space>
2021-03-19 19:55:00 -0500

    Add metaInfo function to dynamically set meta data for twitter card use

--- src/pages/Artwork.vue
+++ src/pages/Artwork.vue
@@ -4,14 +4,6 @@
       <q-img
         :src="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}`"
       />
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:site" content="@site_username">
-      <meta name="twitter:title" content="Top 10 Things Ever">
-      <meta name="twitter:description" content="Up than 200 characters.">
-      <meta name="twitter:creator" content="@creator_username">
-      <!-- <meta name="twitter:image" content="http://placekitten.com/250/250"> -->
-      <meta name="twitter:image" :content="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}`">
-      <meta name="twitter:domain" :content="`https://images-barz-bot.s3.us-east-2.amazonaws.com/${spotifyUser}/${imageId}`">
     </q-card>
   </q-page>
 </template>
@@ -19,6 +11,42 @@
 <script>
 export default {
   name: 'Artwork',
+  metaInfo () {
+    const su = this.$route.params.spotifyUser
+    const imgId = this.$route.params.imageId
+    return {
+      meta: [
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        },
+        // {
+        //   name: 'twitter:site',
+        //   content: '@site_username'
+        // },
+        {
+          name: 'twitter:title',
+          content: 'Created Using BarzBot!'
+        },
+        {
+          name: 'twitter:description',
+          content: 'Song Name - Artist'
+        },
+        // {
+        //   name: 'twitter:creator',
+        //   content: '@creator_username'
+        // },
+        {
+          name: 'twitter:image',
+          content: `https://images-barz-bot.s3.us-east-2.amazonaws.com/${su}/${imgId}`
+        },
+        {
+          name: 'twitter:domain',
+          content: `https://s3.us-east-2.amazonaws.com/webapp-barz-bot/index.html#/artwork/${su}/${imgId}`
+        }
+      ]
+    }
+  },
   created () {
     this.spotifyUser = this.$route.params.spotifyUser
     this.imageId = this.$route.params.imageId
