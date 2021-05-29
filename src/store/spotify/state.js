ef88e986004d5186c3a38fb6ef494a8e96b36efd
flunkout-dvlpr <info@juliogonzalez.space>
2021-05-29 02:42:38 -0500

    Update redirect url for local dev

--- src/store/spotify/state.js
+++ src/store/spotify/state.js
@@ -1,6 +1,6 @@
 export default function () {
   return {
-    redirect_uri: process.env.PROD ? 'https://www.barzbot.app/#' : 'http://localhost:8081/',
+    redirect_uri: process.env.PROD ? 'https://www.barzbot.app/#' : 'http://localhost:8080/',
     client_id: 'ae6a056b5a2244b8b30e0a2cc3300c44',
     client_secret: '31ffe8ba96ea4b8aa7f67071c8e92d85',
     refreshToken: null,
